ac3f6bbdea5b73c9ba41f6be8322f5391a8e2030
Kirill Ki <kirillkalapov@gmail.com>
2021-11-22 22:54:30 +0100

    Initial Commit

---
+++ CocktailShaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocktailShaker
+//
+//  Created by  Mr.Ki on 22.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
