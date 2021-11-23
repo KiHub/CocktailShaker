fed3f14e9a67e2a8d81799c8f652229b40007d8a
Kirill Ki <kirillkalapov@gmail.com>
2021-11-23 11:11:03 +0100

    Story board done

---
+++ CocktailShaker/RecipeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecipeViewController.swift
+//  CocktailShaker
+//
+//  Created by  Mr.Ki on 23.11.2021.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Today you should try:"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
